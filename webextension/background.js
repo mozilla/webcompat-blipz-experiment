4dff47e17137c3fb1745345e4bde7434ba29dcdc
Thomas Wisniewski <wisniewskit@gmail.com>
2018-05-14 19:31:25 -0400

    Minor tweaks to page action behavior/code #55

--- webextension/background.js
+++ webextension/background.js
@@ -138,6 +138,17 @@ const Config = (function() {
       return undefined;
     }
 
+    isPromptableURL(url) {
+      if (!(url instanceof URL)) {
+        try {
+          url = new URL(url);
+        } catch (_) {
+          return false;
+        }
+      }
+      return ["http:", "https:"].includes(url.protocol);
+    }
+
     shouldPromptUserNow(domain) {
       // Prompt at most once a day, at a minimum once every three days,
       // for a maximum of five prompts per user and one prompt per domain.
@@ -202,7 +213,7 @@ async function shouldPromptUser(navDetails) {
   try {
     const url = new URL(navDetails.url);
     return !Config.neverShowAgain &&
-           ["http:", "https:"].includes(url.protocol) &&
+           Config.isPromptableURL(url) &&
            Config.shouldPromptUserNow(url.host) &&
            (!Config.skipPrivateBrowsingTabs ||
             !(await browser.tabs.get(navDetails.tabId)).incognito);
@@ -224,7 +235,6 @@ const portToPageAction = (function() {
     port.onMessage.addListener(onMessageFromPageAction);
     port.onDisconnect.addListener(function() {
       port = undefined;
-      gCurrentlyPromptingTab = undefined;
       TabState.get().then(tabState => {
         // When the popup is hidden.
         updatePageActionIcon(tabState.tabId);
@@ -399,8 +409,9 @@ const TabState = (function() {
 
 async function onTabChanged(info) {
   const { tabId } = info;
+  const { url } = await browser.tabs.get(tabId);
 
-  if (Config.neverShowAgain) {
+  if (Config.neverShowAgain || !Config.isPromptableURL(url)) {
     browser.pageAction.hide(tabId);
     return;
   }
@@ -412,7 +423,7 @@ async function onTabChanged(info) {
   }
 
   if ((gCurrentlyPromptingTab || {}).id === tabId) {
-    await showPageAction(tabId);
+    await popupPageAction(tabId);
 
     const tabState = await TabState.get(tabId);
     tabState.maybeUpdatePageAction();
@@ -437,7 +448,8 @@ async function onNavigationCommitted(navDetails) {
   }
 
   // Show the page action icon if it's been shown before.
-  if (!Config.neverShowAgain && Config.lastPromptTime) {
+  if (!Config.neverShowAgain && Config.lastPromptTime &&
+      Config.isPromptableURL(url)) {
     updatePageActionIcon(tabId);
     await browser.pageAction.show(tabId);
   }
@@ -456,7 +468,7 @@ async function onNavigationCompleted(navDetails) {
     gCurrentlyPromptingTab = {id: tabId, url};
     await updatePageActionIcon(tabId);
     await browser.pageAction.show(tabId);
-    showPageAction(tabId);
+    popupPageAction(tabId);
     Config.onUserPrompted(new URL(url).host);
   }
 }
@@ -602,7 +614,7 @@ function getActiveTab() {
   });
 }
 
-async function showPageAction(tabId) {
+async function popupPageAction(tabId) {
   return browser.experiments.pageAction.forceOpenPopup();
 }
 
